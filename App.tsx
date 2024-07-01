7eeabdb11c6a96585c1fac31d1f2eb0b57c7a0f7
Ayush Kumar <ayush222006@yahoo.com>
2024-07-01 14:31:53 +0530

    devember

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up aop.tsx to start working on your app!</Text>
+      <Text>rahul</Text>
       <StatusBar style="auto" />
     </View>
   );
